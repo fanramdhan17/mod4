31c89d68ae7380bd0f61bd1de1db3ccff0bdb181
luthfanrw17 <olioaglio55@gmail.com>
2022-03-19 14:01:05 +0700

    menambahkan class Penjumlahan dengan method

--- modul4_1302204038/modul4_1302204038/Program.cs
+++ modul4_1302204038/modul4_1302204038/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul4_1302204038
+{
+    public class Penjumlahan<T>
+    {
+        public static T JumlahTigaAngka<T>(T x, T y, T z)
+        {
+            dynamic a = x;
+            dynamic b = y;
+            dynamic c = z;
+
+            return a + b + c;
+        }
+    }
+}
